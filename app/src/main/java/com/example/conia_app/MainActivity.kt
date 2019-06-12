df8a7ad16a81680871279b62dd00413b1ced84ea
Javier Martinez <00034315@uca.edu.sv>
2019-06-11 23:09:31 -0600

    first commit

---
+++ app/src/main/java/com/example/conia_app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.conia_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
+    }
+}
