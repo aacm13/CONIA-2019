e6455d1394e73c99748f5b454938f98c461053d5
Javier Martinez <00034315@uca.edu.sv>
2019-06-22 12:26:31 -0600

    Se eliminó  la actividad y xml CreateAccount y se corrigieron coas en el fragmento createAccountFragment, el proyecto se conectó con firebase

--- app/src/main/java/com/martinez/conia_app/Fragments/createAccountFragment.kt
+++ app/src/main/java/com/martinez/conia_app/Fragments/createAccountFragment.kt
@@ -1,12 +1,15 @@
 package com.martinez.conia_app.Fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.martinez.conia_app.MainActivity
 import com.martinez.conia_app.R
+import kotlinx.android.synthetic.main.fragment_create_account.view.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -40,7 +43,14 @@ class createAccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_account, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_create_account, container, false)
+
+        view.btn_create.setOnClickListener{
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
 
--- app/src/main/java/com/martinez/conia_app/Fragments/loginFragment.kt
+++ app/src/main/java/com/martinez/conia_app/Fragments/loginFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.martinez.conia_app.CreateAccount
 import com.martinez.conia_app.MainActivity
 
 import com.martinez.conia_app.R
